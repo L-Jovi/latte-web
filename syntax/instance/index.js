daf5cb208358dd866ccb9a1f44cb4e087fb9bb73
Eternal_Jovi <eternal.l.jovi@gmail.com>
2019-07-16 14:50:36 +0800

    feat(js syntax): Add prototype and instanceof implements.

---
+++ syntax/instance/index.js
@@ -0,0 +1,39 @@
+// new
+function myNew() {
+    let obj = {}
+    let Constructor = [].shift.call(arguments)
+    obj.__proto__ = Constructor.prototype
+
+    let result = Constructor.apply(obj, arguments)
+    return result instanceof Object ? result : obj
+}
+
+
+// instanceof
+function myInstanceof(left, right) {
+    let prototype = right.prototype
+    left = left.__proto__
+
+    while(true) {
+        if (left === null || left == undefined) {
+            return false
+        }
+        if (prototype === left) {
+            return true
+        }
+        left = left.__proto__
+    }
+}
+
+
+// test
+function CustomFn() {
+    this.name = 'saber'
+}
+
+CustomFn.prototype.fire = function() {
+    console.log('fire')
+}
+
+const instance = myNew(CustomFn)
+console.log(instance)
