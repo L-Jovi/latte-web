bda59dc7d31e868413c00fa55b4830d8a12a8dd8
Eternal_Jovi <eternal.l.jovi@gmail.com>
2019-08-07 00:05:06 +0800

    feat(event): Add event delegate.

--- event/event.js
+++ event/event.js
@@ -8,3 +8,11 @@ child.addEventListener('click', (e) => {
     console.log('fire child')
     e.stopImmediatePropagation()
 })
+
+
+// event delegate
+// with no need for alloc or destroy children node event
+let ul = document.querySelector('#ul')
+ul.addEventListener('click', (event) => {
+    console.log(event.target)
+})
