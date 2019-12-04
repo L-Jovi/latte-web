edfeab08e0699af4b440bebb24bac89b87518347
Eternal_Jovi <eternal.l.jovi@gmail.com>
2019-12-04 22:54:05 +0800

    chore(fix promise test case):

--- es6/promise/index.js
+++ es6/promise/index.js
@@ -1,8 +1,13 @@
+/*
+ * @Description: Promise test case.
+ * @FileName: promise/index.js
+ */
+
 const PromiseSimple = require('./simple')
 const PromiseFull = require('./promise-a+')
 
 
-const s = new PromiseSimple(function(resolve, reject) {
+/* const s = new PromiseSimple(function(resolve, reject) {
   setTimeout(function() {
     const value = 3
     resolve(3)
@@ -31,4 +36,26 @@ p
   })
   .then(function(value) {
     console.log(value)
+  }) */
+
+
+// test real promise exec order
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 2000)
+})
+
+promise
+  .then((val) => {
+    console.log('1st call: ', val)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(2)
+      }, 3000)
+    })
+  })
+  .then((val) => {
+    console.log('2nd call: ', val)
+    return val
   })
