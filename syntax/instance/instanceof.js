b2428f0608bdd7720c09a900d4592bfc8e117a5b
Eternal_Jovi <eternal.l.jovi@gmail.com>
2021-02-03 18:02:38 +0800

    feat: add instanceof

--- syntax/instance/instanceof.js
+++ syntax/instance/instanceof.js
@@ -3,31 +3,20 @@
  * @FileName: instanceof.js
  */
 
-// instanceof
-function myInstanceof(left, right) {
-    let prototype = right.prototype
-    left = left.__proto__
+function forgeInstanceof(left, right) {
+  let prototype = right.prototype
+  left = left.__proto__
 
-    while(true) {
-        if (left === null || left == undefined) {
-            return false
-        }
-        if (prototype === left) {
-            return true
-        }
-        left = left.__proto__
+  while (true) {
+    if (left === null || left == undefined) {
+      return false
     }
+    if (prototype === left) {
+      return true
+    }
+    left = left.__proto__
+  }
 }
 
 
 // main
-function CustomFn() {
-    this.name = 'saber'
-}
-
-CustomFn.prototype.fire = function() {
-    console.log('fire')
-}
-
-const instance = myNew(CustomFn)
-console.log(instance)
