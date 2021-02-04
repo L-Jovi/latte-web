ab6f1ec5ccb8ddc81eec7294a9140f9c744bc614
Eternal_Jovi <eternal.l.jovi@gmail.com>
2021-02-04 22:00:35 +0800

    feat: add library by webpack without babel

---
+++ build/webpack/library/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+
+  entry: './src/index.js',
+
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_',
+    },
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack-numbers.js',
+    library: 'webpackNumbers',
+    libraryTarget: 'umd',
+  },
+}
